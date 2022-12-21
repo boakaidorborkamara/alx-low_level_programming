3f70b2b5b3b2c9939fe44a1baf43f673576d7f77
boakaidorborkamara <boakaidkamara@gmail.com>
2022-12-21 06:46:38 -0800

    Change the style of comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) //A program that prints a string usning printif 
+/**
+ * main - Entry point
+ * Description:  A program that prints a string using printif  function
+ * Return: Return zero for success
+*/
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
