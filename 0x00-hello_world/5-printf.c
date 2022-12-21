6281f56fa12715e76bce3b2487b70e2bc1634291
boakaidorborkamara <boakaidkamara@gmail.com>
2022-12-21 05:05:24 -0800

    Change position of comment

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 
-/**
- * Entry point - main
- * Description: A C program that prints strings follow by a newline
- * Return: return zero as success
-*/
-
-int main(void)
+int main(void) //A program that prints a string usning printif 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
