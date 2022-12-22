ee4142a585ad4ae566633fcbe2dde1e432fba6a1
boakaidorborkamara <boakaidkamara@gmail.com>
2022-12-22 05:04:54 -0800

    Replace an with a for words that are not vowel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@
 
 int main(void)
 {
-	printf("Size of an char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of an int: %ld byte(s)\n", sizeof(int));
-	printf("Size of an long int: %ld byte(s)\n", sizeof(long int));
-	printf("Size of an long long: %ld byte(s)\n", sizeof(long long));
-	printf("Size of an float: %ld byte(s)\n", sizeof(float));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long: %ld byte(s)\n", sizeof(long long));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 
 	return (0);
 
