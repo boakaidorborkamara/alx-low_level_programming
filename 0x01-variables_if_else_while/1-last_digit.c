50c9b5960fb87a90c55ef7dc6e696592948c2bca
boakaidorborkamara <boakaidkamara@gmail.com>
2023-01-01 10:30:45 -0800

    Fix but using the modular operator, change operand

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,13 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int last_digit = n % n;
+	int last_digit = n % 10;
 	
 	if(last_digit > 5)
 	{
 		printf("Last digit of %d is greater than 5", n);
 	}
-	else if(last_digit === 0)
+	else if(last_digit == 0)
 	{
 		printf("Last digit of %d is 0", n);
 	}
