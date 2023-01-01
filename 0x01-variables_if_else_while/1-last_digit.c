3f1d8e5c9b23329e824f2d5057d7cd5c7447d084
boakaidorborkamara <boakaidkamara@gmail.com>
2023-01-01 10:14:23 -0800

    Add condition statement checking the current state of the last digit of the random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,5 +9,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	return (0);
+	int last_digit = n % n;
+	
+	if(last_digit > 5)
+	{
+		printf("Last digit of %d is greater than 5", n);
+	}
+	else if(last_digit === 0)
+	{
+		printf("Last digit of %d is 0", n);
+	}
+	else if(last_digit < 6)
+	{
+		print("Last digit of %d is less than 6 and not 0", n);
+	}
+
 }
