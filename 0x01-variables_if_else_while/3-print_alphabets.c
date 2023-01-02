1a3f76f7d3d0426371daea79fca933b7e20fe8fd
boakaidorborkamara <boakaidkamara@gmail.com>
2023-01-01 20:19:34 -0800

    Create new file and printing uppercase alphabetical character

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char character;
+
+	for(character = 'A'; character <= 'Z'; character++)
+	{
+		putchar(character);
+	}	
+ 	
+	printf("\n");                                                                               
+
+	return (0);
+}	
+
