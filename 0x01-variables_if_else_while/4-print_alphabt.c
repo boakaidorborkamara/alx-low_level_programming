6cc9b2a62f3ddf7de31ef3a8757b35d951eba3e7
boakaidorborkamara <boakaidkamara@gmail.com>
2023-01-01 20:43:53 -0800

    create a new file and print lowercase letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char character;
+
+	for(character = 'a'; character <= 'z'; character++)
+	{
+		if(character !== 'e' || character !== "q")
+		{
+			putchar(character);
+		}	
+	}	
+ 	
+	printf("\n");                                                                               
+
+	return (0);
+}	
+
